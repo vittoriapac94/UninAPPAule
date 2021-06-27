753beef9db07aa8424b47c78da926df41e688ff1
Vittoria <vittoriapacchiano@yahoo.it>
2021-06-27 11:57:19 +0200

    Commit application

--- authentication_service/src/main/java/com/vipac/authentication_service/services/MatricolaNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.vipac.authentication_service.services;
-
-public class MatricolaNotFoundException extends Exception {
-	
-	public MatricolaNotFoundException(String msg) {
-		super(msg);
-	}
-
-}
